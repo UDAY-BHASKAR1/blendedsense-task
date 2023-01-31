38027fac4c63aba60b66e7d0fcd09f4405519181
UDAY-BHASKAR1 <udaybhaskarambati@gmail.com>
2023-01-31 18:37:12 +0530

    tuesday chnages

---
+++ src/app/shared/dropdown/dropdown.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.css']
+})
+export class DropdownComponent {
+  @Input() selectedType: string;
+  @Input() TypeList: Array<any>;
+  @Output() selectedItemInfo = new EventEmitter<string>();
+  @Input() editable;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  selectedItem(itemSelected: string) {
+    this.selectedItemInfo.emit(itemSelected);
+  }
+}
